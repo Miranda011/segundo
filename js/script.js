7c29cdbe8d383677a023cf3a88badfd131a567d4
Miranda011 <kaiomiranda0906@gmail.com>
2024-06-18 08:49:22 -0300

    site

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const menuIcon = document.getElementById('menu-icon');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    menuIcon.addEventListener('click', (event) => {
+       
+        event.preventDefault();
+       
+        if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+            dropdownMenu.style.display = 'block';
+        } else {
+            dropdownMenu.style.display = 'none';
+        }
+    });
+
+    
+    window.addEventListener('click', (event) => {
+        if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = 'none';
+        }
+    });
+});
