85a240a788883c912cd83ebe7d4a7cae23712ec6
Miranda011 <kaiomiranda0906@gmail.com>
2024-06-20 11:51:42 -0300

    comprar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', (event) => {
     const menuIcon = document.getElementById('menu-icon');
     const dropdownMenu = document.getElementById('dropdown-menu');
 
     menuIcon.addEventListener('click', (event) => {
-       
+
         event.preventDefault();
-       
+
         if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
             dropdownMenu.style.display = 'block';
         } else {
@@ -14,10 +13,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-    
+
     window.addEventListener('click', (event) => {
         if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.style.display = 'none';
         }
     });
 });
+
+const foto = document.getElementById('tenisGrande')
+
+function trocaFoto(value) {
+    foto.src = `icons/${value}`
+    console.log(value)
+}
